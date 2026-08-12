e0169f7c3e406ad0b9d734bc19fca818f9247c3b
박서현 <=>
2026-08-12 18:52:44 +0900

    Feat: class 나눔

---
+++ main.py
@@ -0,0 +1,61 @@
+# 문제 정의: 컴퓨터로 패턴 구분하기 
+
+class InputData:
+    def __init__(self):
+        pass
+
+    #데이터 수집
+    def input(self):
+        #사용자가 데이터를 입력
+        #몇줄입력할건지선택
+        #열행개수맞나 봄
+        #공백 구별
+        pass
+    
+    def load(self):
+        #json 파일 읽어옴
+        pass
+
+    def save(self):
+        #저장함..
+        #pattern
+        #filter
+        #expected는 있을 수도 없을수도 사이즈별로정리하는게좋수도 아닐수도 
+        pass
+
+
+    #데이터 전처리
+    def norm_label():
+        #라벨 정규화 
+        pass
+
+
+#데이터 분석
+class Analyze:
+    def __init__(self):
+        pass
+
+    def mac_calculate(self):
+        #MAC 계산을 여기서
+        
+        #사이즈 판별하고 list 분리하고
+        size = len()
+
+        pass
+    
+
+class Evaluate:
+    def __init__(self):
+        pass
+
+    def printresult(self):
+        #결과 출력 
+        pass
+
+    def calculate(self):
+        pass
+# 사용자 입력 
+
+# Json 데이터 로드
+
+#
